ce181ad821dd5058eaa183324087935ce5da59e9
Judah3191n <W0509673@nscc.ca>
2025-01-14 10:15:48 -0400

    organized

---
+++ Exercise 2/C Files/Exercise2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double pi = 3.14159;
+float radius = 7.154;
+
+int main(){
+    //does the calculation
+    float circumference = pi*radius*2;
+    int intCircumference = circumference;
+    //initalize a double to write in scientific notation
+    double doubleCircumference = circumference;
+    
+    //in the placeholder put %.2f cuts the float down to 2 decimal places
+    printf("The radius of the circle with 2 decimal places: %.2f\n",radius);
+
+    //%.3f makes the float only have 3 decimal places
+    printf("The floating-point circumference with 3 decimal places: %.3f\n",circumference);
+
+    printf("The integer-converted circumference as an integer: %d", intCircumference);
+
+    //%e writes a double as scientific notation
+    printf("Planck’s constant in scientific notation: %e",doubleCircumference);
+    
+    return 0;
+}
